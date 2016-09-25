2364356cd682b4c15469c657a2e341506ca4a7ec
Jeremy Fields <jeremy.fields@viget.com>
2016-09-25 14:31:02 -0600

    Update for better resource usage

--- dist/focuser.js
+++ dist/focuser.js
@@ -1,6 +1,6 @@
 /**
  * focuser - Ditch those ugly focus styles and use Focuser! A stylable, traveling focus indicator.
- * @version v1.0.2
+ * @version v1.0.3
  * @link https://github.com/ten1seven/focuser
  * @license MIT
  */
@@ -62,9 +62,35 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	module.exports = (function() {
 
+	  // object containing the focuser's current dimensions
+	  var focuserBox = {
+	    width: 0,
+	    height: 0,
+	    top: 0,
+	    left: 0
+	  };
+
+	  // block-level elements
+	  // that should not receive a focus indicator
+	  var formInputs = [
+	    'body',
+	    'div',
+	    'form',
+	    'main',
+	    'nav'
+	  ];
+
+	  // empty variable for holding the curretly-focused element
 	  var currentElem = null;
-	  var focusElem = null;
-	  var focusElemPadding = 5;
+
+	  // empty variable for the focuser element
+	  var focuserElem = null;
+
+	  // extra "padding" between focuser and the element it's focusing
+	  var focuserElemPadding = 5;
+
+	  // boolean containing whether something is currently focused
+	  var isFocused = false;
 
 	  /*
 	    --------------------
@@ -73,40 +99,56 @@ return /******/ (function(modules) { // webpackBootstrap
 	  */
 
 	  var createFocuser = function() {
-	    focusElem = document.createElement('div');
-	    focusElem.classList.add('a11y-focuser');
+	    focuserElem = document.createElement('div');
+	    focuserElem.classList.add('a11y-focuser');
 
-	    document.body.appendChild(focusElem);
+	    document.body.appendChild(focuserElem);
 	  };
 
 	  var updateFocuser = function() {
-	    if (currentElem) {
+	    if (isFocused && currentElem) {
 	      var elemInfo = currentElem.getBoundingClientRect();
 
+	      // reasons to stop an active focuser
 	      if (
+
+	        // if the current element is one of the block level elements
+	        (formInputs.indexOf(currentElem.nodeName.toLowerCase()) !== -1) ||
+
+	        // OR the element is less than 1px in height and width
+	        // this catches elements that may be using accessible techniques for hidden content
+	        // via: https://snook.ca/archives/html_and_css/hiding-content-for-accessibility
 	        (
-	          currentElem.nodeName.toLowerCase() === 'body' ||
-	          currentElem.nodeName.toLowerCase() === 'div' ||
-	          currentElem.nodeName.toLowerCase() === 'form' ||
-	          currentElem.nodeName.toLowerCase() === 'main' ||
-	          currentElem.nodeName.toLowerCase() === 'nav'
-	        ) || (
 	          elemInfo.height <= 1 &&
 	          elemInfo.width <= 1
 	        )
 	      ) {
-	        currentElem = null;
-	        focusElem.classList.remove('-focus');
-	      } else {
-	        focusElem.style.height = (elemInfo.height + (focusElemPadding * 2)) + 'px';
-	        focusElem.style.width = (elemInfo.width + (focusElemPadding * 2)) + 'px';
-	        focusElem.style.left = (elemInfo.left + window.pageXOffset - focusElemPadding) + 'px';
-	        focusElem.style.top = (elemInfo.top + window.pageYOffset - focusElemPadding) + 'px';
-	        focusElem.classList.add('-focus');
-	      }
-	    }
+	        stopFocuser();
 
-	    requestAnimationFrame(updateFocuser);
+	      // only update the focuser element if
+	      // the dimensions have changed to save on processing
+	      } else if (
+	        focuserBox.width !== elemInfo.width ||
+	        focuserBox.height !== elemInfo.height ||
+	        focuserBox.top !== elemInfo.top ||
+	        focuserBox.left !== elemInfo.left
+	      ) {
+
+	        // save the current dimensions for comparison
+	        focuserBox.width = elemInfo.width;
+	        focuserBox.height = elemInfo.height;
+	        focuserBox.top = elemInfo.top;
+	        focuserBox.left = elemInfo.left;
+
+	        // set the style on the focuser
+	        focuserElem.style.width = (elemInfo.width + (focuserElemPadding * 2)) + 'px';
+	        focuserElem.style.height = (elemInfo.height + (focuserElemPadding * 2)) + 'px';
+	        focuserElem.style.top = (elemInfo.top + window.pageYOffset - focuserElemPadding) + 'px';
+	        focuserElem.style.left = (elemInfo.left + window.pageXOffset - focuserElemPadding) + 'px';
+	      }
+
+	      requestAnimationFrame(updateFocuser);
+	    }
 	  };
 
 
@@ -116,14 +158,25 @@ return /******/ (function(modules) { // webpackBootstrap
 	    --------------------
 	  */
 
-	  var toggleFocuser = function(event) {
-	    currentElem = (event.type === 'focusin') ? event.target : null;
-	    focusElem.classList[(event.type === 'focusin') ? 'add' : 'remove']('-focus');
+	  var startFocuser = function(event) {
+	    isFocused = true;
+	    currentElem = event.target;
+
+	    focuserElem.classList.add('-focus');
+
+	    updateFocuser();
+	  };
+
+	  var stopFocuser = function() {
+	    isFocused = false;
+	    currentElem = null;
+
+	    focuserElem.classList.remove('-focus');
 	  };
 
 	  var addListeners = function() {
-	    window.addEventListener('focusin', toggleFocuser);
-	    window.addEventListener('focusout', toggleFocuser);
+	    document.body.addEventListener('focusin', startFocuser);
+	    document.body.addEventListener('focusout', stopFocuser);
 	  };
 
 
--- src/focuser.js
+++ src/focuser.js
@@ -1,8 +1,34 @@
 module.exports = (function() {
 
+  // object containing the focuser's current dimensions
+  var focuserBox = {
+    width: 0,
+    height: 0,
+    top: 0,
+    left: 0
+  };
+
+  // block-level elements
+  // that should not receive a focus indicator
+  var formInputs = [
+    'body',
+    'div',
+    'form',
+    'main',
+    'nav'
+  ];
+
+  // empty variable for holding the curretly-focused element
   var currentElem = null;
-  var focusElem = null;
-  var focusElemPadding = 5;
+
+  // empty variable for the focuser element
+  var focuserElem = null;
+
+  // extra "padding" between focuser and the element it's focusing
+  var focuserElemPadding = 5;
+
+  // boolean containing whether something is currently focused
+  var isFocused = false;
 
   /*
     --------------------
@@ -11,40 +37,56 @@ module.exports = (function() {
   */
 
   var createFocuser = function() {
-    focusElem = document.createElement('div');
-    focusElem.classList.add('a11y-focuser');
+    focuserElem = document.createElement('div');
+    focuserElem.classList.add('a11y-focuser');
 
-    document.body.appendChild(focusElem);
+    document.body.appendChild(focuserElem);
   };
 
   var updateFocuser = function() {
-    if (currentElem) {
+    if (isFocused && currentElem) {
       var elemInfo = currentElem.getBoundingClientRect();
 
+      // reasons to stop an active focuser
       if (
+
+        // if the current element is one of the block level elements
+        (formInputs.indexOf(currentElem.nodeName.toLowerCase()) !== -1) ||
+
+        // OR the element is less than 1px in height and width
+        // this catches elements that may be using accessible techniques for hidden content
+        // via: https://snook.ca/archives/html_and_css/hiding-content-for-accessibility
         (
-          currentElem.nodeName.toLowerCase() === 'body' ||
-          currentElem.nodeName.toLowerCase() === 'div' ||
-          currentElem.nodeName.toLowerCase() === 'form' ||
-          currentElem.nodeName.toLowerCase() === 'main' ||
-          currentElem.nodeName.toLowerCase() === 'nav'
-        ) || (
           elemInfo.height <= 1 &&
           elemInfo.width <= 1
         )
       ) {
-        currentElem = null;
-        focusElem.classList.remove('-focus');
-      } else {
-        focusElem.style.height = (elemInfo.height + (focusElemPadding * 2)) + 'px';
-        focusElem.style.width = (elemInfo.width + (focusElemPadding * 2)) + 'px';
-        focusElem.style.left = (elemInfo.left + window.pageXOffset - focusElemPadding) + 'px';
-        focusElem.style.top = (elemInfo.top + window.pageYOffset - focusElemPadding) + 'px';
-        focusElem.classList.add('-focus');
-      }
-    }
+        stopFocuser();
 
-    requestAnimationFrame(updateFocuser);
+      // only update the focuser element if
+      // the dimensions have changed to save on processing
+      } else if (
+        focuserBox.width !== elemInfo.width ||
+        focuserBox.height !== elemInfo.height ||
+        focuserBox.top !== elemInfo.top ||
+        focuserBox.left !== elemInfo.left
+      ) {
+
+        // save the current dimensions for comparison
+        focuserBox.width = elemInfo.width;
+        focuserBox.height = elemInfo.height;
+        focuserBox.top = elemInfo.top;
+        focuserBox.left = elemInfo.left;
+
+        // set the style on the focuser
+        focuserElem.style.width = (elemInfo.width + (focuserElemPadding * 2)) + 'px';
+        focuserElem.style.height = (elemInfo.height + (focuserElemPadding * 2)) + 'px';
+        focuserElem.style.top = (elemInfo.top + window.pageYOffset - focuserElemPadding) + 'px';
+        focuserElem.style.left = (elemInfo.left + window.pageXOffset - focuserElemPadding) + 'px';
+      }
+
+      requestAnimationFrame(updateFocuser);
+    }
   };
 
 
@@ -54,14 +96,25 @@ module.exports = (function() {
     --------------------
   */
 
-  var toggleFocuser = function(event) {
-    currentElem = (event.type === 'focusin') ? event.target : null;
-    focusElem.classList[(event.type === 'focusin') ? 'add' : 'remove']('-focus');
+  var startFocuser = function(event) {
+    isFocused = true;
+    currentElem = event.target;
+
+    focuserElem.classList.add('-focus');
+
+    updateFocuser();
+  };
+
+  var stopFocuser = function() {
+    isFocused = false;
+    currentElem = null;
+
+    focuserElem.classList.remove('-focus');
   };
 
   var addListeners = function() {
-    window.addEventListener('focusin', toggleFocuser);
-    window.addEventListener('focusout', toggleFocuser);
+    document.body.addEventListener('focusin', startFocuser);
+    document.body.addEventListener('focusout', stopFocuser);
   };
 
 
